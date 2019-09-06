ca10f931d8edfd7eb7d27adbe64fcb7b9d5ef4e0
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-09-06 11:37:34 +0100

    Amoah: ref #1 added calc.py to solve this issue

---
+++ calc.py
@@ -0,0 +1,10 @@
+#calc.py A python program that print the first, second and third year of an initial balance
+#of $1000 and earns 5 percent per a year
+#Author: Amoah Joseph
+#Date 5th september, 2019
+
+print(1000)
+print(1000* 0.05+1000)
+print(1050* 0.05+1000)
+print(1052.5* 0.05+1000)
+print(1052.625* 0.05+1000)
