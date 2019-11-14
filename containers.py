d8d1920b6b7e719aec3c051bf5a93b8cbba7febc
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:37:10 +0000

    Amoah: ref #5 Added containers.py to solve this issue

---
+++ containers.py
@@ -0,0 +1,9 @@
+# A Python program tha compute and displaying the refund and returning containers
+less = 0.10
+more = 0.25
+less_cans = input("How many less containers did you recycle?")
+more_cans = input("How many more containers did you recycle?")
+less_cansPrices = less * int(less_cans)
+more_cansPrice = more * int(more_cans)
+totalRefund = int(less_cans) + int(more_cans)
+print("The total refund of the bottles is $" + str(totalRefund) + ".")
