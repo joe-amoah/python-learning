25dc3d7ac8d393f47a39dc5fbe44b08a48cd7a96
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:49:58 +0000

    Amoah: ref #12 Added Distance Earth.py to solve this issue

---
+++ Distance Earth.py
@@ -0,0 +1,26 @@
+# Python program that allows the user to enter the latitude and longitude of two points on the Earth in degrees.
+from math import radians, cos, sin, asin, sqrt
+def distance (lat1, lat2, lon1, lon2):
+
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+
+    c = 2 * asin(sqrt(a))
+
+    r = 6371
+
+    return(c * r)
+
+
+lat1 = 53.32055555555556
+lat2 = 53.31861111111111
+lon1 = -1.7297222222222221
+lon2 = -1.69972222222222223
+print(distance(lat1, lat2, lon1, lon2), 'K.M')
+
