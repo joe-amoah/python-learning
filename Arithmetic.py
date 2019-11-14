b95b3c7deff6c8a128de8e5842d7e909c1b56566
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:45:03 +0000

    Amoah: ref #10 Added Arithmetic.py to solve this issue

---
+++ Arithmetic.py
@@ -0,0 +1,10 @@
+# Python program that reads two integers, a and b, from the user
+import math
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+print("The sum of a and b is", a+b)
+print("The subtraction of b and a", b-a)
+print("The quotient when a is divided by b", a/b)
+print("The remainder when a is divided by b", a//b)
+print("The result od log10 a",math.log(a))
+print("The result of a and b",a**b)
