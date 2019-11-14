bdea1bc3b0a7fd546aa3f8766c9ea46fb58b944f
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:38:43 +0000

    Amoah: ref #6 Added Mealtax.py to solve this issue

---
+++ Mealtax.py
@@ -0,0 +1,7 @@
+# Python program that calculate the tax amount,tip amount and the grand total
+# of a meal ordered at the restaurant by a customer or user
+MealPrice = float(input("what is the price of the meal"))
+Tip = MealPrice * 0.18
+Tax = MealPrice * 0.12
+MealPriceTotal = MealPrice + Tip + Tax
+print("The total amount of this meal is", float (str(round)))
