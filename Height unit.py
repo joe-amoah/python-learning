b0b1c4b0fa3c7b44038acd3e21fd841c1382acf0
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:54:18 +0000

    Amoah: ref #14 Added Heiht unit.py to solve this issue

---
+++ Height unit.py
@@ -0,0 +1,7 @@
+# Python program that reads a number of feet and inches from the user
+
+cm = int(input("Enter the height in centimetres:"))
+inches = 2.54 * cm
+feet = 12 * cm
+print("The length in inches", round(inches,2))
+print("The length in feet", round(inches,2))
