720b2385964ac2142b2b39f75d9142b25e8d60a5
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:40:12 +0000

    Amoah: ref #7 Added Positive Integers.py to solve this issue

---
+++ Positive Integers.py
@@ -0,0 +1,5 @@
+# Python program that calculate the sum of First N positive integers
+
+n = int(input("Enter a positive integers"))
+total = n * (n+1) / 2
+print("The sum of the First" , n, "positive integers", total)
