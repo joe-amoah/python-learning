65d3a1f0ace884cad241b8979f4b1baebe0b7e24
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:43:23 +0000

    Amoah: ref #9 Added compoundinterest.py to solve this issue

---
+++ compoundinterest.py
@@ -0,0 +1,9 @@
+# A Python program that begins by reading the amount of money deposited into the account from the user.
+p = float(input("Enter the principal amount:"))
+r = float(input("Enter the interest rate:"))
+t = float(input("Enter the time in years:"))
+amount = 1000* 0.4 / 3
+print("Compound amount is %. 2f" % round())
+print("Compound interest is %. 2f" % round())
+
+
