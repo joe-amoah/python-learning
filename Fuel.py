cbb9edd559ccc31441ec705e3627f047a84daad3
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-14 22:46:11 +0000

    Amoah: ref #11 Added fuel.py to solve this issue

---
+++ Fuel.py
@@ -0,0 +1,9 @@
+# Python program that reads a value from the user in American
+# units and displays the equivalent fuel efficiency in Canadian units.
+
+miles_per_gallon = int(input("Enter the number of fuel use in America"))
+liters_kilometres = int(input("Enter the number of fuel use in Canada "))
+
+mpg = liters_kilometres / 100
+print("The  number of fuel use is", 'America, unit')
+print("The number of fuel use is", 'Canada, unit')
