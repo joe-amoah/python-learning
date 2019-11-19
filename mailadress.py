d4a62fc671dc43b892b2acc41bc05cc7328bacd4
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-19 11:54:48 +0000

    Amoah: ref #4 Added mailadress.py to solve this issue

--- mailadress.py
+++ mailadress.py
@@ -1,4 +1,5 @@
-
-Name = "Joseph Amoah"
-Address = "Box Ms 686 Mile 7 Accra"
-print("f{Name} + {Address}")
+# A python program that display a complete Address format and does not read any input from the user
+print("Joseph Amoah")
+print("Box Ms 686 New Achimota")
+print("Accra, Ghana")
+print("20th October, 2019")
