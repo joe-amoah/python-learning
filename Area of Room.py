c13f6be3b615cc348c4c56369b3d58ff39381c50
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-19 13:16:31 +0000

    Amoah: ref #6 Added Area of Room.py to solve this issue

--- Area of Room.py
+++ Area of Room.py
@@ -1,4 +1,4 @@
-# A Python Program that ask the user to enter the width and length of a room
+# A Python Program that ask the user to enter the width and length of a room.
 width = float(input("please enter the height:"))
 length = float(input("please enter the length:"))
 area = width*length
