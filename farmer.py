8c524527a0fbc5a2edce19a28c73ba38443aef6e
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-19 13:10:43 +0000

    Amoah: ref #7 Added famer.py to solve this issue

--- farmer.py
+++ farmer.py
@@ -1,4 +1,4 @@
-# A python program that reads the length and width of a farmer field from the user in feet
+# A python program that reads the length and width of a farmer field from the user in feet.
 # and then display the area of the field in acres
 
 length = float(input("Enter the length of the field in feet"))
