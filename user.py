69871c6c1aebe053a7cd8bcd26bcfac4dfbd6001
joe-amoah <mr.nyamekeamoah@gmail.com>
2019-11-19 12:22:26 +0000

    Amoah: ref #5 Added user.py to solve this issue

--- user.py
+++ user.py
@@ -1,3 +1,3 @@
-# A Python program that greet the user an ask him or her to write the name
+# A Python program that greet the user an ask him or her to write the name.
 user_text = input("what is your name")
 print("hello", user_text)
